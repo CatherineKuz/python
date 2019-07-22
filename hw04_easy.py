ccf625b0cec8dae1fd58f087ffc44d48e3d3deab
Catherine <kate.poisonglen@gmail.com>
2019-07-22 16:44:20 +0300

    Update hw04_easy.py

--- hw04_easy.py
+++ hw04_easy.py
@@ -4,10 +4,9 @@
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
-# my_list = [1, 2, 3, 4]
-# new = [el**2 for el in my_list]
-# print(new)
-
+my_list = [1, 2, 3, 4]
+new = [el**2 for el in my_list]
+print(new)
 
 
 
@@ -15,16 +14,13 @@
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
 
-# fruit1 = ["apple", "banana", "grapes"]
-# fruit2 = ["banana", "pineapple", "grapes", "pear"]
-# fruit_new = [el for el in fruit1 if el == el in fruit2]
-# print(fruit_new)
+fruit1 = ["apple", "banana", "grapes"]
+fruit2 = ["banana", "pineapple", "grapes", "pear"]
+fruit_new = [el for el in fruit1 if el == el in fruit2]
+print(fruit_new)
 
-# fruit_new = list(set(fruit1)&set(fruit2))
-# print(fruit_new)
-
-
-
+fruit_new = list(set(fruit1)&set(fruit2))
+print(fruit_new)
 
 
 
@@ -35,9 +31,9 @@
 # + Элемент положительный
 # + Элемент не кратен 4
 
-# my_list = [1, 2, 3, 4, 5, 6, -3, -7, 8, 9, 12, 0]
-# new = [el for el in my_list if el > 0 and el%3 == 0 and el%4 != 0]
-# print(new)
+my_list = [1, 2, 3, 4, 5, 6, -3, -7, 8, 9, 12, 0]
+new = [el for el in my_list if el > 0 and el%3 == 0 and el%4 != 0]
+print(new)
 
 
 
